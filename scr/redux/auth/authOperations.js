0ccfc9c98227a798249569ff5313af951d501eb1
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2023-05-09 23:22:01 +0300

    add operations and reducer

--- scr/redux/auth/authOperations.js
+++ scr/redux/auth/authOperations.js
@@ -1,12 +1,28 @@
 import { auth } from "../../firebase/config";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { authSlice } from "./authReducer";
+const { updateUserProfile } = authSlice.actions;
 
 
 export const authSignUpUser = ({ email, password, login }) => async (dispatch, getState) => {
     console.log("email, password, nickname", email, password, login);
     try {
-       const user = await createUserWithEmailAndPassword(auth, email, password);
-    //    console.log('user', user);
+       await createUserWithEmailAndPassword(auth, email, password);
+       const user = await auth.currentUser;
+        console.log(user)
+        const { displayName, uid } = await auth.currentUser;
+
+        await updateProfile(user, {
+            displayName: login,
+          });
+
+        dispatch(updateUserProfile({
+            userId: uid,
+            login: displayName,
+        }))
+    //    dispatch(authSlice.actions.updateUserProfile({ userId: user.uid }))
+       console.log('user', user);
+       
     } catch (error) {
         console.log("error", error);
         console.log("error.message", error.message);
@@ -25,4 +41,6 @@ export const authSignInUser = ({ email, password }) => async (dispatch, getState
 };
 
 export const authSignOutUser = () => async (dispatch, getState) => {};
+
+export const authStateChange = () => async (dispatch, getState) => {};
 
--- scr/redux/auth/authReducer.js
+++ scr/redux/auth/authReducer.js
@@ -5,6 +5,18 @@ export const authSlice = createSlice({
   initialState: {
     userId: null,
     login: null,
+    stateChange: null,
   },
-  reducers: {},
+  reducers: {
+    updateUserProfile: (state, { payload }) => ({
+      ...state,
+      userId: payload.userId,
+      login: payload.login
+    }),
+    authStateChange: (state, { payload }) => ({
+      ...state,
+      stateChange: payload.stateChange,
+    }),
+  },
 });
+// console.log(authSlice);
